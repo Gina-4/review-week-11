9b4b807f314b3f4ee7ba29804e66ccf73254e9f7
Gina-4 <Georgina.Riri4@gmail.com>
2025-04-03 15:36:21 +1300

    added comments

--- index.js
+++ index.js
@@ -15,22 +15,25 @@ const inputText = document.getElementById('inputText')
 const favButton = document.getElementById('favButton')
 const favColorBox = document.getElementById('favColorBox')
 
-
+// button function 
   selectButton.addEventListener('click', function() {
+    // input text value to display color
     const text = document.getElementById('inputText').value;
     console.log(text)
+    // styling
     inputValue.style.backgroundColor = text;
     });
     
-
+// button function
 randomSelect.addEventListener('click', function() {
+  // random color from array
  const randomIndex = Math.floor(Math.random() * randomColors.length);
     console.log(randomColors[randomIndex]); 
   inputValue.style.backgroundColor = randomColors[randomIndex]; 
   favColorBox.style.color = "blue";
 
   });
-
+// button function with styling
   favButton.addEventListener('click', function() {
 favColorBox.style.backgroundColor = "orange";
 favColorBox.style.color = "red";
